46792e8761525bd3ec20d1731fb7515a38ee8644
興怡 <>
2024-08-04 01:31:47 -0700

    courses api

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -4,6 +4,38 @@
 export async function onRequestPost(context) {
   try {
     let input = await context.request.formData();
+    // Convert FormData to JSON
+    // NOTE: Allows multiple values per key
+    let output = {};
+    for (let [key, value] of input) {
+      let tmp = output[key];
+      if (tmp === undefined) {
+        output[key] = value;
+      } else {
+        output[key] = [].concat(tmp, value);
+      }
+    }
+
+    let data = JSON.stringify(output);
+    // submit input to our mongo backed api
+    const api = context.env.AZURE_COSMOS_URL + "/courses";
+
+    // TODO is the Cf-Access header included?
+    const res = await fetch(api, {
+        method: "POST",
+        body: data,
+    });
+    return res;
+
+
+  } catch (err) {
+    return new Response('Error parsing JSON content', { status: 400 });
+  }
+}
+/*******************************
+export async function OLDonRequestPost(context) {
+  try {
+    let input = await context.request.formData();
 
     // Convert FormData to JSON
     // NOTE: Allows multiple values per key
@@ -27,3 +59,5 @@ export async function onRequestPost(context) {
     return new Response('Error parsing JSON content', { status: 400 });
   }
 }
+******************************************/
+
