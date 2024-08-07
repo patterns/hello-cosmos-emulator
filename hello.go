3b772657a5a9e83a282ca8358dfec3b5a1807460
興怡 <>
2024-08-07 06:57:43 -0700

    users list

--- hello.go
+++ hello.go
@@ -14,59 +14,117 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// globals , TODO move into env vars?
-// //var emuuri string = "mongodb://localhost:C2y6yDjf5%2FR%2Bob0N8A7Cgv30VRDJIWEHLM%2B4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw%2FJw%3D%3D@localhost:10255/admin?ssl=true"
-const (
-	localuri             = "mongodb://localhost:C2y6yDjf5%2FR%2Bob0N8A7Cgv30VRDJIWEHLM%2B4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw%2FJw%3D%3D@localhost:10255/admin?tls=false"
-	MongoDBDatabaseEnv   = "MONGODB_DATABASE"
-	MongoDBCollectionEnv = "MONGODB_COLLECTION"
-)
-
-var (
-	database   string
-	collection string
-)
+var mg MongoInstance
 
 func main() {
-	////client := connect()
-	////log.Printf("mongo sessions, %d", client.NumberSessionsInProgress())
+	mg = connect()
+	defer mg.C.Disconnect(mg.Ctx)
 
 	app := fiber.New()
-	app.Post("/api/v1/courses", add)
+	app.Get("/api/v1/users", users)
+	app.Post("/api/v1/users", join)
+	app.Get("/api/v1/courses", courses)
+	app.Post("/api/v1/courses", compose)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	app.Get("/", numSessions)
 	app.Listen(":3000")
 }
+//TODO middleware to validate CF Access JWT
 
-// api handler, add
-func add(c *fiber.Ctx) error {
-	course := new(Course)
-	c.BodyParser(course)
-	oid, err := createCourse(c, course)
-	if err != nil {
-		return c.Status(500).JSON(err)
-	}
-
-	inserted, err := findCourse(c, oid)
-	if err != nil {
-		return c.Status(500).JSON(err)
-	}
-
-	return c.Status(200).JSON(inserted)
+// api handler, users list
+func users(c *fiber.Ctx) error {
+	return vanilla[User](c, "users")
 }
 
-// data access, open connection to db
-func connect() *mongo.Client {
-	database = os.Getenv(MongoDBDatabaseEnv)
-	if database == "" {
-		log.Fatalf("missing environment variable, %s", MongoDBDatabaseEnv)
+// api handler, courses list
+func courses(c *fiber.Ctx) error {
+	return vanilla[Course](c, "courses")
+}
+
+// null handler, session count
+func numSessions(c *fiber.Ctx) error {
+	kv := map[string]int{"count": mg.C.NumberSessionsInProgress()}
+	return c.JSON(kv)
+}
+
+// vanilla handler, list
+func vanilla[T Resource](c *fiber.Ctx, collection string) error {
+	// get all records as a cursor
+	query := bson.D{{}}
+	cursor, err := mg.D.Collection(collection).Find(c.Context(), query)
+	if err != nil {
+		return c.Status(500).SendString(err.Error())
 	}
-	// TODO dynamic collection from route
-	collection = os.Getenv(MongoDBCollectionEnv)
-	if collection == "" {
-		log.Fatalf("missing environment variable, %s", MongoDBCollectionEnv)
+
+	var rows []T = make([]T, 0)
+
+	// iterate the cursor and decode each item into desired items
+	if err := cursor.All(c.Context(), &rows); err != nil {
+		return c.Status(500).SendString(err.Error())
+	}
+	// return list in JSON format
+	return c.JSON(rows)
+}
+
+// api handler, compose (course)
+func compose(c *fiber.Ctx) error {
+	input := new(Course)
+	c.BodyParser(input)
+	fresh := &Course{
+		ID:          primitive.NewObjectID(),
+		Title:       input.Title,
+		Description: input.Description,
+		URL:         input.URL,
+		Published:   time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC),
+	}
+
+	oid, err := create(c, fresh, "courses")
+	if err != nil {
+		return c.Status(500).JSON(err)
+	}
+
+	saved, err := find(c, oid, "courses")
+	if err != nil {
+		return c.Status(500).JSON(err)
+	}
+
+	return c.Status(200).JSON(saved)
+}
+
+// api handler, join (user)
+func join(c *fiber.Ctx) error {
+	parsed := new(User)
+	c.BodyParser(parsed)
+	fresh := &User{
+		ID:          primitive.NewObjectID(),
+		Email:       parsed.Email,
+		Name:        parsed.Name,
+		Role:        parsed.Role,
+		Created:     time.Now().UTC(),
+		Deactivated: time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC),
+	}
+
+	oid, err := create(c, fresh, "users")
+	if err != nil {
+		return c.Status(500).JSON(err)
+	}
+
+	saved, err := find(c, oid, "users")
+	if err != nil {
+		return c.Status(500).JSON(err)
+	}
+
+	return c.Status(200).JSON(saved)
+}
+
+// ///////////////////////////////////////
+// config/database: data access
+
+// open connection to db
+func connect() MongoInstance {
+	databaseName := os.Getenv(mongoEnv)
+	if databaseName == "" {
+		log.Fatalf("missing environment variable, %s", mongoEnv)
 	}
 
 	// wait 10 seconds to connect, otherwise give up
@@ -75,6 +133,7 @@ func connect() *mongo.Client {
 	// skip TLS verify while testing on local emulator
 	var cfg = &tls.Config{InsecureSkipVerify: true}
 	opts := options.Client().ApplyURI(localuri).SetDirect(true)
+	opts.SetRetryWrites(false)
 	opts.SetTLSConfig(cfg)
 	c, err := mongo.NewClient(opts)
 
@@ -85,47 +144,74 @@ func connect() *mongo.Client {
 		log.Fatalf("Ping failed, %v", err)
 	}
 
-	return c
+	return MongoInstance{
+		C:   c,
+		D:   c.Database(databaseName),
+		Ctx: ctx,
+	}
 }
 
 // data access, insert
-func createCourse(c *fiber.Ctx, course *Course) (primitive.ObjectID, error) {
-	client := connect()
-	defer client.Disconnect(c.Context())
-
-	courseCollection := client.Database(database).Collection(collection)
-	r, err := courseCollection.InsertOne(c.Context(), course)
+func create(c *fiber.Ctx, fresh interface{}, collection string) (primitive.ObjectID, error) {
+	coll := mg.D.Collection(collection)
+	r, err := coll.InsertOne(c.Context(), fresh)
 	if err != nil {
-		log.Printf("Add course failed ")
+		log.Printf("Add item failed ")
 		return primitive.NilObjectID, err
 	}
-
+	// assert result ID
 	oid := r.InsertedID.(primitive.ObjectID)
-	log.Printf("Course added, %s", oid.Hex())
+	log.Printf("Item added, %s", oid.Hex())
 	return oid, nil
 }
 
-func findCourse(c *fiber.Ctx, oid primitive.ObjectID) (bson.M, error) {
-	client := connect()
-	defer client.Disconnect(c.Context())
-	courseCollection := client.Database(database).Collection(collection)
+// data access, get by id
+func find(c *fiber.Ctx, oid primitive.ObjectID, collection string) (bson.M, error) {
+	coll := mg.D.Collection(collection)
 
-	var inserted bson.M
+	var found bson.M
 	query := bson.D{{"_id", oid}}
 
-	err := courseCollection.FindOne(c.Context(), query).Decode(&inserted)
+	err := coll.FindOne(c.Context(), query).Decode(&found)
 	if err != nil {
 		return nil, err
 	}
 
-	return inserted, nil
+	return found, nil
 }
 
-type Course struct {
-	ID          primitive.ObjectID `bson:"_id"`
-	CreatedAt   time.Time          `bson:"created_at"`
-	PublishedAt time.Time          `bson:"Published_at"`
-	Title       string             `bson:"title"`
-	Description string             `bson:"description"`
-	URL         string             `bson:"url"`
+// ///////////////////////////////////////
+// type constraint for union of courses and users
+type Resource interface {
+	User | Course
 }
+type User struct {
+	ID          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Created     time.Time          `json:"created" bson:"created"`
+	Deactivated time.Time          `json:"deactivated" bson:"deactivated"`
+	Email       string             `json:"email" bson:"email"`
+	Name        string             `json:"name" bson:"name"`
+	Role        string             `json:"role" bson:"role"`
+}
+type Course struct {
+	ID          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Published   time.Time          `json:"published" bson:"published"`
+	Title       string             `json:"title" bson:"title"`
+	Description string             `json:"description" bson:"description"`
+	URL         string             `json:"url" bson:"url"`
+}
+
+// MongoInstance contains the Mongo client and database objects
+type MongoInstance struct {
+	// todo Is this struct just for namespace?
+	C   *mongo.Client
+	D   *mongo.Database
+	Ctx context.Context
+}
+
+// globals , TODO move into env vars?
+// //var emuuri string = "mongodb://localhost:C2y6yDjf5%2FR%2Bob0N8A7Cgv30VRDJIWEHLM%2B4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw%2FJw%3D%3D@localhost:10255/admin?ssl=true"
+const (
+	localuri = "mongodb://localhost:C2y6yDjf5%2FR%2Bob0N8A7Cgv30VRDJIWEHLM%2B4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw%2FJw%3D%3D@localhost:10255/admin?tls=false"
+	mongoEnv = "MONGODB_DATABASE"
+)
